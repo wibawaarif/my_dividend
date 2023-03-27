ec81630ca48cc5328173fe744e4128b6c09a70c9
wibawaarif <arifwb.work@gmail.com>
2023-03-27 23:01:14 +0700

    Initialize app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+<v-app>
+   <v-app-bar app dark flat class="px-12">
+      <v-btn >
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon> MyDividend
+      </v-btn>
+      
+  
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')" class="text-yellow">Dashboard</v-btn>
+      <v-btn text @click="scroll('about')"  >Upcoming Dividends</v-btn>
+    </v-app-bar>
+</v-app>
+</template>
+
+<script>
+ export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="pa-6" fluid>
+  <Navbar />
+</v-container>
+</template>
+
+<script setup>
+  import Navbar from '@/components/Navbar.vue'
+</script>
