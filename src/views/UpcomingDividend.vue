6905b1f56617139a37ce5c01540aaccc913a19b5
wibawaarif <arifwb.work@gmail.com>
2023-05-18 16:52:07 +0700

    Update UpcomingDividend.vue

--- src/views/UpcomingDividend.vue
+++ src/views/UpcomingDividend.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="px-0 py-0" fluid>
+  <div>
     <Navbar type="dashboard" />
     <v-app style="padding-left: 100px; padding-right: 100px">
       <v-breadcrumbs class="px-0" :items="items" />
@@ -11,51 +11,128 @@
           >
 
       <div class="d-flex justify-start">
-      <v-tabs v-model="tab" color="indigo-accent-2" align-tabs="center">
+      <!-- <v-tabs v-model="tab" color="indigo-accent-2" align-tabs="center">
         <v-tab class="mr-2">Summary</v-tab>
         <v-tab class="mr-2">Arif</v-tab>
-      </v-tabs>
+      </v-tabs> -->
     </div>
 
-    <v-divider></v-divider>
-
+    <v-divider class="mt-3"></v-divider>
+     
 
     <v-card class="my-10 mx-auto">
-      <div class="py-4 ml-5">
-        <span class="my-10 font-weight-medium">Expected Dividends</span>
-      </div>
-
-      <v-divider></v-divider>
-      <div>
-        <apexchart
-         height="500"
-          type="line"
-          :options="options"
-          :series="series"
-        ></apexchart>
-      </div>
+      <div class="d-flex justify-center" >
+      <calendar-heatmap style="width: 1000px;" tooltip-unit="dividends" :end-date="endDate" :values="values"/>
+    </div>
     </v-card>
 
-    <v-card class="my-10 mx-auto">
+    <v-card>
+      <v-card-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search Holdings"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        v-model:items-per-page="itemsPerPage"
+        :headers="headers"
+        :items="getholdings"
+        item-value="name"
+        class="elevation-1"
+        :search="search"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-btn
+            class="mr-2"
+            @click="editItem(item.raw)"
+            size="small"
+            icon="mdi-pencil"
+          >
+          </v-btn>
+          <v-btn @click="deleteItem(item.raw.id, item.raw.symbol)" size="small" icon="mdi-delete">
+          </v-btn>
+        </template>
+        <template v-slot:item.profitLoss="{ item }">
+          <span :class="item.raw.profitLoss < 0 ? 'text-red' : 'text-green'">{{ item.raw.profitLoss }}</span>
+        </template>
+      </v-data-table>
+      <!-- <div v-else class="q-pa-md">
+    <q-markup-table>
+      <thead>
+        <tr>
+          <th class="text-left" style="width: 150px">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+          <th class="text-right">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+          <th class="text-right">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+          <th class="text-right">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+          <th class="text-right">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+          <th class="text-right">
+            <q-skeleton animation="blink" type="text" />
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="n in 5" :key="n">
+          <td class="text-left">
+            <q-skeleton animation="blink" type="text" width="85px" />
+          </td>
+          <td class="text-right">
+            <q-skeleton animation="blink" type="text" width="50px" />
+          </td>
+          <td class="text-right">
+            <q-skeleton animation="blink" type="text" width="35px" />
+          </td>
+          <td class="text-right">
+            <q-skeleton animation="blink" type="text" width="65px" />
+          </td>
+          <td class="text-right">
+            <q-skeleton animation="blink" type="text" width="25px" />
+          </td>
+          <td class="text-right">
+            <q-skeleton animation="blink" type="text" width="85px" />
+          </td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </div> -->
+    </v-card>
+
+    <!-- <v-card class="my-10 mx-auto">
       <div style="height: 75px; width: 50%;" class="d-flex align-center px-6 justify-between">
          <span style="font-weight: 500; font-size: 18px;">Average: $90</span>
          <span style="font-weight: 500; font-size: 18px;">Highest: $184</span>
          <span style="font-weight: 500; font-size: 18px;">Lowest: 33</span>
          <span style="font-weight: 500; font-size: 18px;">Variance: 100%</span>
       </div>
-    </v-card>
+    </v-card> -->
 
     </div>
     </v-app>
-  </v-container>
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import { CalendarHeatmap } from 'vue3-calendar-heatmap'
 
 export default {
   components: {
     Navbar,
+    CalendarHeatmap
   },
   data() {
     return {
@@ -80,6 +157,17 @@ export default {
           categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
         },
       },
+      headers: [
+        {
+          title: "Ticker",
+          align: "start",
+          key: "symbol",
+        },
+        { title: "Amount", align: "end", key: "amount" },
+        { title: "Ex Date", align: "end", key: "exDate" },
+        { title: "Record Date", align: "end", key: "recordDate" },
+        { title: "Description", align: "end", key: "description", sortable: false },
+      ],
       series: [
         {
           name: "series-1",
@@ -88,10 +176,38 @@ export default {
       ],
     };
   },
+  computed: {
+    endDate() {
+      const end = "2023-05-18"
+      return end
+    },
+    values() {
+      const value = [{
+        count: 45, 
+        date: "2023-05-16"
+      }, {
+        count: 30, 
+        date: "2023-05-15"
+      }]
+      return value
+    },
+  },
   created() {
     document.title = "Upcoming Dividends | MyDividend";
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style>
+.vch__legend {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: end;
+}
+
+.vch__days__labels__wrapper {
+  visibility: hidden;
+}
+
+</style>
